4b91f28f260dd6cfa1b31dc758252c00fc35625e
alex <sashaka00120@mail.ru>
2022-05-19 12:38:00 +0300

    feat: complete task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const { stdin, stdout,stderr } = process;
+const path = require('path');
+const pafthFile = path.join(__dirname, 'text.txt');
+
+console.log('Привет. Как ты ?');
+
+// const input = fs.createReadStream('source.txt', 'utf-8');
+const output = fs.createWriteStream(pafthFile);
+
+// input.pipe(output);
+
+stdin.on('data', data => {
+
+  output.write(data);
+
+  const dataString=data.toString().split('');
+  const newData= dataString.splice(0, dataString.length-2).join('');
+
+  if (newData=='exit'){
+    process.exit();
+  }
+  
+});
+
+process.on('exit', () => stdout.write('Удачи!'));
+process.on('SIGINT', () => {
+process.exit();});
